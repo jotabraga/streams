9801da9a60c829b85930f2107ff593ee86c9c511
jotabraga <jpmbraga.feg@gmail.com>
2023-11-30 22:36:57 -0300

    feat: create read stream by native node function

--- readable-writable/read-big-files-in-chinks.mjs
+++ readable-writable/read-big-files-in-chinks.mjs
@@ -1,7 +1,7 @@
 //for i in `seq 1 10`; do node -e "process.stdout.write('hello world'.repeat(1e7))" >> 1gbbig.file; done
 
 import { promises, createReadStream, statSync } from 'node:fs';
-const filename = './1gbbig.file'
+const filename = './big.file'
 
 try {
     const file = await promises.readFile(filename);
@@ -13,3 +13,25 @@ try {
 
 const { size } = statSync(filename);
 console.log('file size', size / 1e9, "GB", "\n");
+
+let chunkConsumed = 0;
+
+const stream = createReadStream(filename) //65k per readable
+.once('data', msg => {
+    console.log('on data length', msg.toString().length);
+})
+.once('readable', _ => {
+    console.log('read 11 chunk bytes', stream.read(11).toString())
+    console.log('read 05 chunk bytes', stream.read(5).toString())
+    chunkConsumed += 11 + 5;
+    //this stream.read(11) will trigger the on(data) event
+})
+.on('readable', _ => {
+    let chunk;
+    while(null !== (chunk = stream.read())) {
+        chunkConsumed += chunk.length;
+    }
+})
+.on('end', () => {
+    console.log(`Read ${chunkConsumed / 1e9} bytes of data`)
+})
