eb3faef8be766db212161bcc26d61561f520c932
jotabraga <jpmbraga.feg@gmail.com>
2024-01-16 11:56:58 -0300

    feat: create broadcaster server

---
+++ project-chat/server.mjs
@@ -0,0 +1,15 @@
+import net from "node:net";
+import { randomUUID } from "node:crypto";
+const users = new Map();
+
+const server = net.createServer((socket) => {
+  socket.pipe(socket);
+});
+
+server.listen(3000, () => console.log("Server is runnig at 3000"));
+
+server.on("connection", (socket) => {
+  socket.id = randomUUID();
+  console.log("new connection!", socket.id);
+  users.set(socket.id, socket);
+});
